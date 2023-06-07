1226054f55c5a93b07785d5050dfb0ed3e0c4d6c
Amr Elsadany <elsadany_94@hotmail.com>
2023-06-07 18:57:55 +0300

    First Commit

---
+++ FindAndCopyDamagedImages/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using ImageProcessor;
+using System.Configuration;
+using System.Drawing;
+
+DirectoryInfo dir = new DirectoryInfo(ConfigurationManager.AppSettings["CurrentImageFolder"]);
+
+FileInfo[] imageFiles = dir.GetFiles("*."+ ConfigurationManager.AppSettings["ImageType"]);
+int count = 0;
+
+foreach (FileInfo f in imageFiles)
+{
+    try
+    {
+        ImageFactory factory = new ImageFactory();
+        factory.Load(f.FullName);
+    }
+    catch (Exception ex) {
+        var destPath = ConfigurationManager.AppSettings["DestImageFolder"];
+        System.IO.File.Copy(f.FullName, Path.Combine(destPath, f.Name));
+        Console.WriteLine("Corrupted File Name Proccessed : " + f.Name);
+        count++;
+    }
+}
+
+Console.WriteLine(count);
